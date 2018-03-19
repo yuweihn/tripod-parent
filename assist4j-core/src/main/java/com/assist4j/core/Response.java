302187f1381a84e579067fe5444f24c9f608ae25
yuwei <yuwei@yuweix.com>
2018-03-19 16:28:20 +0800

    1.0.5

--- assist4j-core/src/main/java/com/assist4j/core/Response.java
+++ assist4j-core/src/main/java/com/assist4j/core/Response.java
@@ -11,7 +11,7 @@ public class Response<T> implements Serializable {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 2124472245885834223L;
+	private static final long serialVersionUID = 1L;
 	private String code;
 	private String msg;
 	private T data;
