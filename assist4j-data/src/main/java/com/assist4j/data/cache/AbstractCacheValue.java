7f03fc6bb440ed19387f8773c5512c0df7997454
yuwei <yuwei@yuweix.com>
2018-04-02 11:55:56 +0800

    1.0.5.3

--- assist4j-data/src/main/java/com/assist4j/data/cache/AbstractCacheValue.java
+++ assist4j-data/src/main/java/com/assist4j/data/cache/AbstractCacheValue.java
@@ -9,11 +9,12 @@ import org.springframework.beans.BeanUtils;
  * @author yuwei
  */
 public abstract class AbstractCacheValue<T extends AbstractCacheValue<T>> implements CacheValue<T> {
+	@Override
 	public String encode() {
 		return JSONObject.toJSONString(this);
 	}
 
-
+	@Override
 	public T decode(String value) {
 		Object bean = JSONObject.parseObject(value, this.getClass());
 		try {
@@ -22,5 +23,15 @@ public abstract class AbstractCacheValue<T extends AbstractCacheValue<T>> implem
 			throw new RuntimeException(e);
 		}
 		return (T) this;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
 	}
 }
