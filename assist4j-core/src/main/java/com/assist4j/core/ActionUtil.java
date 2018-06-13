16fae0e9a4fdc9b0c12ef93cb8cbd3a2b475753f
yuwei <yuwei@yuweix.com>
2018-06-13 17:20:29 +0800

    1.0.11

--- assist4j-core/src/main/java/com/assist4j/core/ActionUtil.java
+++ assist4j-core/src/main/java/com/assist4j/core/ActionUtil.java
@@ -28,20 +28,20 @@ public abstract class ActionUtil {
 	public static String getRequestIP() {
 		HttpServletRequest request = getRequest();
 		String ip = request.getHeader("X-Real-IP");
-		if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("x-forwarded-for");
 		}
-		if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
-		if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
 		}
-		if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
 		}
 
-		if(ip == null) {
+		if (ip == null) {
 			return null;
 		}
 		return ip.split(",")[0];
