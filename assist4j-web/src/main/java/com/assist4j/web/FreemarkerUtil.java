edbebbe05af1fda462d4c96bcca477965f67b0b5
yuwei <yu.wei@niuhome.com>
2018-06-23 22:03:59 +0800

    1.0.13

--- assist4j-web/src/main/java/com/assist4j/web/FreemarkerUtil.java
+++ assist4j-web/src/main/java/com/assist4j/web/FreemarkerUtil.java
@@ -3,10 +3,18 @@ package com.assist4j.web;
 
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.HashMap;
 import java.util.Map;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.RequestContext;
+import org.springframework.web.servlet.view.AbstractTemplateView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -15,6 +23,7 @@ import freemarker.template.Template;
 public class FreemarkerUtil {
 	private static Configuration cfg;
 	private static String ftlPath = "";
+	private static boolean exposeSpringMacroHelpers = false;
 
 	private FreemarkerUtil() {
 
@@ -25,6 +34,16 @@ public class FreemarkerUtil {
 		return merge(template, null);
 	}
 	public static String merge(String template, Map<String, Object> params) {
+		if (params == null) {
+			params = new HashMap<String, Object>();
+		}
+		if (exposeSpringMacroHelpers) {
+			HttpServletRequest request = getRequest();
+			HttpServletResponse response = getResponse();
+			params.put(AbstractTemplateView.SPRING_MACRO_REQUEST_CONTEXT_ATTRIBUTE, new RequestContext(request
+					, response, request.getServletContext(), params));
+		}
+
 		try {
 			Template temp = cfg.getTemplate(ftlPath + template);
 
@@ -37,6 +56,16 @@ public class FreemarkerUtil {
 		}
 	}
 
+	private static HttpServletRequest getRequest() {
+		ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		return sra == null ? null : sra.getRequest();
+	}
+
+	private static HttpServletResponse getResponse() {
+		ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		return sra == null ? null : sra.getResponse();
+	}
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public void setCfg(Configuration cfg) {
 		FreemarkerUtil.cfg = cfg;
@@ -44,5 +73,9 @@ public class FreemarkerUtil {
 
 	public void setFtlPath(String ftlPath) {
 		FreemarkerUtil.ftlPath = ftlPath;
+	}
+
+	public void setExposeSpringMacroHelpers(boolean exposeSpringMacroHelpers) {
+		FreemarkerUtil.exposeSpringMacroHelpers = exposeSpringMacroHelpers;
 	}
 }
