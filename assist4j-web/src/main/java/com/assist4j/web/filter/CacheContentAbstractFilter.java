6199385da038e4b069239990fb043977a3813106
yuwei <yuwei@yuweix.com>
2018-09-07 16:05:38 +0800

    1.1.4

---
+++ assist4j-web/src/main/java/com/assist4j/web/filter/CacheContentAbstractFilter.java
@@ -0,0 +1,29 @@
+package com.assist4j.web.filter;
+
+
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * @author yuwei
+ */
+public class CacheContentAbstractFilter extends AbstractFilter<ContentCachingRequestWrapper, ContentCachingResponseWrapper> {
+    @Override
+    protected ContentCachingRequestWrapper wrap(HttpServletRequest request) {
+        return new ContentCachingRequestWrapper(request);
+    }
+
+    @Override
+    protected ContentCachingResponseWrapper wrap(HttpServletResponse response) {
+        return new ContentCachingResponseWrapper(response);
+    }
+
+    @Override
+    protected void printRequest(ContentCachingRequestWrapper request) {
+        super.printRequest(request);
+    }
+}
