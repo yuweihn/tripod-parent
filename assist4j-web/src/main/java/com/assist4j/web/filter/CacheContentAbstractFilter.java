c12c523c3a190e083b6d5690f0801a7fa8d85286
yuwei <yuwei@yuweix.com>
2018-09-07 16:24:12 +0800

    1.1.4

--- assist4j-web/src/main/java/com/assist4j/web/filter/CacheContentAbstractFilter.java
+++ assist4j-web/src/main/java/com/assist4j/web/filter/CacheContentAbstractFilter.java
@@ -1,6 +1,8 @@
 package com.assist4j.web.filter;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
@@ -12,6 +14,16 @@ import javax.servlet.http.HttpServletResponse;
  * @author yuwei
  */
 public class CacheContentAbstractFilter extends AbstractFilter<ContentCachingRequestWrapper, ContentCachingResponseWrapper> {
+    private static final Logger log = LoggerFactory.getLogger(CacheContentAbstractFilter.class);
+
+    private boolean logBody = false;
+
+
+    public void setLogBody(boolean logBody) {
+        this.logBody = logBody;
+    }
+
+
     @Override
     protected ContentCachingRequestWrapper wrap(HttpServletRequest request) {
         return new ContentCachingRequestWrapper(request);
@@ -25,5 +37,13 @@ public class CacheContentAbstractFilter extends AbstractFilter<ContentCachingReq
     @Override
     protected void printRequest(ContentCachingRequestWrapper request) {
         super.printRequest(request);
+
+        if (logBody) {
+            byte[] bytes = request.getContentAsByteArray();
+            if (bytes != null && bytes.length > 0) {
+                String content = new String(bytes);
+                log.info("body: {}", content);
+            }
+        }
     }
 }
