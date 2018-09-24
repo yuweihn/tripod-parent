7bc94ee7ab82ed865702297e31bb5a58d5f9f042
yuwei <yu.wei@niuhome.com>
2018-09-24 10:02:01 +0800

    1.1.6

--- assist4j-spring-boot-starter/src/main/java/com/assist4j/boot/WebAutoConfiguration.java
+++ assist4j-spring-boot-starter/src/main/java/com/assist4j/boot/WebAutoConfiguration.java
@@ -1,24 +1,18 @@
 package com.assist4j.boot;
 
 
-import com.assist4j.core.exception.ExceptionHandler;
 import com.assist4j.web.TextUtil;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.lang.reflect.Constructor;
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
@@ -55,18 +49,5 @@ public class WebAutoConfiguration {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("bundles.message");
         return messageSource;
-    }
-
-    @ConditionalOnProperty(name = "assist4j.boot.web.exceptionHandler.enabled", matchIfMissing = true)
-    @ConditionalOnMissingBean(name = "exceptionHandler")
-    @Bean(name = "exceptionHandler")
-    public HandlerExceptionResolver handlerExceptionResolver(@Value("${error.page:}") String errorPage) {
-        Map<Class<?>, String> errorMsgMap = new HashMap<Class<?>, String>();
-        errorMsgMap.put(MaxUploadSizeExceededException.class, "上传文件太大");
-
-        ExceptionHandler exceptionHandler = new ExceptionHandler();
-        exceptionHandler.setErrorPage(errorPage);
-        exceptionHandler.setErrorMsgMap(errorMsgMap);
-        return exceptionHandler;
     }
 }
