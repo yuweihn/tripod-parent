2ef079a693a2e72cef78124d3ab5c2dfe5438f76
yuwei <yuwei@yuweix.com>
2018-10-19 11:24:02 +0800

    1.1.11

--- assist4j-sequence/src/main/java/com/assist4j/sequence/base/SequenceBeanHolder.java
+++ assist4j-sequence/src/main/java/com/assist4j/sequence/base/SequenceBeanHolder.java
@@ -14,7 +14,7 @@ public interface SequenceBeanHolder {
 	 * 值：seqName,minValue
 	 *
 	 * 示例：seqUser: seq_user,100
-	 * 表示注册到到Spring容器中的Bean实例以seqUser为beanName，同时存入数据库的记录的名字为seq_user，并保证最小值为100
+	 * 表示注册到Spring容器中的Bean实例以seqUser为beanName，同时存入数据库的记录的名字为seq_user，并保证最小值为100
 	 */
 	Map<String, String> getBeanSeqNameMap();
 }
