c87fc278d084ef745c09ec4ffca480e8c59e94d3
yuwei <yuwei@yuweix.com>
2018-11-01 17:29:03 +0800

    1.1.13

--- assist4j-dao/src/main/java/com/assist4j/dao/mybatis/where/Criteria.java
+++ assist4j-dao/src/main/java/com/assist4j/dao/mybatis/where/Criteria.java
@@ -27,14 +27,17 @@ public class Criteria implements Serializable {
 	}
 
 	public Criteria add(Connector connector, Criterion criterion) {
-		sql.append(" ").append(connector.getCode()).append(" ");
+		String criterionSql = criterion.toSql();
+		if (criterionSql != null && !"".equals(criterionSql.trim())) {
+			sql.append(" ").append(connector.getCode()).append(" ").append(criterionSql).append(" ");
+		}
 		return this;
 	}
 
 	public Criteria add(Connector connector, Criteria criteria) {
 		String criteriaSql = criteria.toSql();
 		if (criteriaSql != null && !"".equals(criteriaSql.trim())) {
-			sql.append(" (").append(criteriaSql).append(") ");
+			sql.append(" ").append(connector.getCode()).append(" (").append(criteriaSql).append(") ");
 		}
 		return this;
 	}
