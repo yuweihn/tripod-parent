9eacbef3b8340ce69090783b784e75cd28fafa8d
yuwei <yuwei@yuweix.com>
2018-12-06 19:44:23 +0800

    1.0.31

--- assist4j-session/src/main/java/com/assist4j/session/filter/HeaderSessionFilter.java
+++ assist4j-session/src/main/java/com/assist4j/session/filter/HeaderSessionFilter.java
@@ -12,32 +12,30 @@ import javax.servlet.http.HttpServletResponse;
  * @author yuwei
  */
 public class HeaderSessionFilter extends SessionFilter {
-	private String key;
+    private String key;
 
 
-	public HeaderSessionFilter(SessionCache cache) {
-		super(cache);
-	}
-	public HeaderSessionFilter() {
-		super();
-	}
+    public HeaderSessionFilter(SessionCache cache) {
+        super(cache);
+    }
+    public HeaderSessionFilter() {
+        super();
+    }
 
-	public void setKey(String key) {
-		this.key = key;
-	}
+    public void setKey(String key) {
+        this.key = key;
+    }
 
+    @Override
+    protected String getSessionId(HttpServletRequest request, HttpServletResponse response) {
+        if (key == null || "".equals(key)) {
+            throw new IllegalArgumentException("Parameter[key] is not initialized.");
+        }
 
-
-	@Override
-	protected String getSessionId(HttpServletRequest request, HttpServletResponse response) {
-		if (key == null || "".equals(key)) {
-			throw new IllegalArgumentException("Parameter[key] is not initialized.");
-		}
-
-		String sessionId = request.getHeader(key);
-		if (sessionId == null || "".equals(sessionId.trim())) {
-			throw new IllegalArgumentException("Required header part '" + key + "' is not present.");
-		}
-		return sessionId;
-	}
+        String sessionId = request.getHeader(key);
+        if (sessionId == null || "".equals(sessionId.trim())) {
+            throw new IllegalArgumentException("Required header part '" + key + "' is not present.");
+        }
+        return sessionId;
+    }
 }
