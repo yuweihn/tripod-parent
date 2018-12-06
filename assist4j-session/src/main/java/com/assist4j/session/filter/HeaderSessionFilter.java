2eaa1264171428b0f22a64a8c1c63f45f1c1fe0b
yuwei <yuwei@yuweix.com>
2018-12-06 19:42:29 +0800

    1.0.31

---
+++ assist4j-session/src/main/java/com/assist4j/session/filter/HeaderSessionFilter.java
@@ -0,0 +1,43 @@
+package com.assist4j.session.filter;
+
+
+import com.assist4j.session.cache.SessionCache;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * 从header中取出某个值作为sessionId
+ * @author yuwei
+ */
+public class HeaderSessionFilter extends SessionFilter {
+	private String key;
+
+
+	public HeaderSessionFilter(SessionCache cache) {
+		super(cache);
+	}
+	public HeaderSessionFilter() {
+		super();
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+
+
+	@Override
+	protected String getSessionId(HttpServletRequest request, HttpServletResponse response) {
+		if (key == null || "".equals(key)) {
+			throw new IllegalArgumentException("Parameter[key] is not initialized.");
+		}
+
+		String sessionId = request.getHeader(key);
+		if (sessionId == null || "".equals(sessionId.trim())) {
+			throw new IllegalArgumentException("Required header part '" + key + "' is not present.");
+		}
+		return sessionId;
+	}
+}
