ed43abf2222d54bb30278d434193773795b89ea0
yuwei <yuwei@yuweix.com>
2019-01-11 17:49:10 +0800

    1.1.17

--- assist4j-schedule/src/main/java/com/assist4j/schedule/base/AbstractThreadPoolPageTask.java
+++ assist4j-schedule/src/main/java/com/assist4j/schedule/base/AbstractThreadPoolPageTask.java
@@ -9,27 +9,16 @@ import java.util.concurrent.ExecutorService;
  * @author yuwei
  */
 public abstract class AbstractThreadPoolPageTask<T> extends AbstractThreadPoolTask<T> {
-	private int pageSize = 20;
-	
-	
 	@Override
 	protected void executeInThreadPool(ExecutorService executor) {
 		while (true) {
-			List<T> taskList = queryTaskList(1, pageSize);
+			List<T> taskList = queryTaskList();
 			if (taskList == null || taskList.size() <= 0) {
 				break;
 			}
 			executeInThreadPool(executor, taskList);
 		}
 	}
-
-
-	@Override
-	protected final List<T> queryTaskList() {
-		throw new RuntimeException("Not Supported.");
-	}
-	
-	protected abstract List<T> queryTaskList(int pageNo, int pageSize);
 }
 
 
