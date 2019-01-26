91c8af0a60c987b63d856da069fd8efd094d8b0d
yuwei <yu.wei@niuhome.com>
2019-01-26 19:17:17 +0800

    1.0.36

--- assist4j-data/src/main/resources/script/getLock.lua
+++ assist4j-data/src/main/resources/script/getLock.lua
@@ -1,6 +1,6 @@
-if redis.call('setNx',KEYS[1],ARGV[1]) then
-    if redis.call('get',KEYS[1])==ARGV[1] then
-        return redis.call('expire',KEYS[1],ARGV[2])
+if redis.call('setNx', KEYS[1], ARGV[1]) then
+    if redis.call('get', KEYS[1]) == ARGV[1] then
+        return redis.call('expire', KEYS[1], ARGV[2])
     else
         return 0
     end
