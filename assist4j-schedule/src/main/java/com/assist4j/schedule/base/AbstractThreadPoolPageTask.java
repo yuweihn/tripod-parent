4f184f21c2469fc15dfcf338e610d1d7964966ec
yuwei <yu.wei@niuhome.com>
2019-05-03 22:15:04 +0800

    1.0.38

--- assist4j-schedule/src/main/java/com/assist4j/schedule/base/AbstractThreadPoolPageTask.java
+++ assist4j-schedule/src/main/java/com/assist4j/schedule/base/AbstractThreadPoolPageTask.java
@@ -9,15 +9,31 @@ import java.util.concurrent.ExecutorService;
  * @author yuwei
  */
 public abstract class AbstractThreadPoolPageTask<T> extends AbstractThreadPoolTask<T> {
+	private static final int DEFAULT_MAX_TIMES = -1;
+
+
 	@Override
 	protected void executeInThreadPool(ExecutorService executor) {
+		int times = 0;
+		int maxTimex = getMaxTimes();
+
 		while (true) {
+			boolean stop = maxTimex != DEFAULT_MAX_TIMES && times > maxTimex;
+			if (stop) {
+				break;
+			}
+
 			List<T> taskList = queryTaskList();
 			if (taskList == null || taskList.size() <= 0) {
 				break;
 			}
 			executeInThreadPool(executor, taskList);
+			times++;
 		}
+	}
+
+	protected int getMaxTimes() {
+		return DEFAULT_MAX_TIMES;
 	}
 }
 
