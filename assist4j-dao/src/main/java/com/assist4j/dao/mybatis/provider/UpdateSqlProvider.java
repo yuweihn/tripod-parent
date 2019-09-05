7959c2aa08243ece5d69e6445d57fcb7ec785ca5
yuwei <yuwei@yuweix.com>
2019-09-05 15:31:05 +0800

    1.1.21

--- assist4j-dao/src/main/java/com/assist4j/dao/mybatis/provider/UpdateSqlProvider.java
+++ assist4j-dao/src/main/java/com/assist4j/dao/mybatis/provider/UpdateSqlProvider.java
@@ -1,6 +1,7 @@
 package com.assist4j.dao.mybatis.provider;
 
 
+import com.assist4j.dao.mybatis.where.Criteria;
 import org.apache.ibatis.jdbc.SQL;
 
 import javax.persistence.Id;
@@ -8,6 +9,7 @@ import javax.persistence.Version;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -15,6 +17,14 @@ import java.util.List;
  */
 public class UpdateSqlProvider extends AbstractProvider {
 	public <T>String updateByPrimaryKey(T t) throws IllegalAccessException {
+		return toUpdateByPrimaryKeySql(t, false);
+	}
+
+	public <T>String updateByPrimaryKeySelective(T t) throws IllegalAccessException {
+		return toUpdateByPrimaryKeySql(t, true);
+	}
+
+	private <T>String toUpdateByPrimaryKeySql(T t, boolean selective) throws IllegalAccessException {
 		Class<?> entityClass = t.getClass();
 		String tableName = getTableName(entityClass);
 
@@ -26,6 +36,13 @@ public class UpdateSqlProvider extends AbstractProvider {
 				Field field = fc.getField();
 				field.setAccessible(true);
 
+				if (selective) {
+					Object o = field.get(t);
+					if (o == null) {
+						continue;
+					}
+				}
+
 				Id idAnn = field.getAnnotation(Id.class);
 				if (idAnn != null) {
 					WHERE("`" + fc.getColumnName() + "` = #{" + field.getName() + "}");
@@ -33,11 +50,11 @@ public class UpdateSqlProvider extends AbstractProvider {
 				} else {
 					SET("`" + fc.getColumnName() + "`" + " = #{" + field.getName() + "} ");
 				}
-				
+
 				Version version = field.getAnnotation(Version.class);
 				if (version != null) {
 					WHERE("`" + fc.getColumnName() + "` = #{" + field.getName() + "}");
-					
+
 					int val = field.getInt(t);
 					SET("`" + fc.getColumnName() + "`" + " = " + (val + 1) + " ");
 				}
@@ -48,41 +65,50 @@ public class UpdateSqlProvider extends AbstractProvider {
 		}}.toString();
 	}
 
-	public <T>String updateByPrimaryKeySelective(T t) throws IllegalAccessException {
-		Class<?> entityClass = t.getClass();
-		String tableName = getTableName(entityClass);
+	public <T>String updateByCriteria(Map<String, Object> param) throws IllegalAccessException {
+		return toUpdateByCriteriaSql(param, false);
+	}
 
+	public <T>String updateByCriteriaSelective(Map<String, Object> param) throws IllegalAccessException {
+		return toUpdateByCriteriaSql(param, true);
+	}
+
+	private <T>String toUpdateByCriteriaSql(Map<String, Object> param, boolean selective) throws IllegalAccessException {
+		T t = (T) param.get("t");
+		Class<?> entityClass = t.getClass();
+		List<String> excludeFields = (List<String>) param.get("excludeFields");
+		Criteria criteria = (Criteria) param.get("criteria");
+		if (criteria == null || criteria.getParams() == null || criteria.getParams().size() <= 0) {
+			throw new IllegalAccessException("'where' is missed.");
+		}
+		String tableName = getTableName(entityClass);
 		List<FieldColumn> fcList = getPersistFieldList(entityClass);
 		return new SQL() {{
 			UPDATE(tableName);
-			boolean whereSet = false;
+			WHERE(criteria.toSql());
 			for (FieldColumn fc: fcList) {
 				Field field = fc.getField();
-				field.setAccessible(true);
-
-				Object o = field.get(t);
-				if (o == null) {
+				if (excludeFields != null && excludeFields.contains(field.getName())) {
 					continue;
 				}
-				
-				Id idAnn = field.getAnnotation(Id.class);
-				if (idAnn != null) {
-					WHERE("`" + fc.getColumnName() + "` = #{" + field.getName() + "}");
-					whereSet = true;
-				} else {
-					SET("`" + fc.getColumnName() + "`" + "= #{" + field.getName() + "} ");
+				field.setAccessible(true);
+
+				if (selective) {
+					Object o = field.get(t);
+					if (o == null) {
+						continue;
+					}
 				}
-				
+
+				SET("`" + fc.getColumnName() + "`" + " = #{t." + field.getName() + "} ");
+
 				Version version = field.getAnnotation(Version.class);
 				if (version != null) {
-					WHERE("`" + fc.getColumnName() + "` = #{" + field.getName() + "}");
-					
+					WHERE("`" + fc.getColumnName() + "` = #{t." + field.getName() + "}");
+
 					int val = field.getInt(t);
 					SET("`" + fc.getColumnName() + "`" + " = " + (val + 1) + " ");
 				}
-			}
-			if (!whereSet) {
-				throw new IllegalAccessException("'where' is missed.");
 			}
 		}}.toString();
 	}
