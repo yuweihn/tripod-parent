17c9629cb71339464564563542e378ba61e9c757
yuwei <yuwei@yuweix.com>
2019-09-18 19:12:24 +0800

    1.0.43

--- assist4j-data/src/main/resources/script/getLockXx.lua
+++ assist4j-data/src/main/resources/script/getLockXx.lua
@@ -1,7 +1,1 @@
-if redis.call('setxx', KEYS[1], ARGV[1]) == 1 then
-    if redis.call('get', KEYS[1]) == ARGV[1] then
-        return redis.call('expire', KEYS[1], ARGV[2])
-    else
-        return 0
-    end
-end
+return redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2], 'XX')
