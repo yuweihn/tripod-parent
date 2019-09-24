e7d20f0143a65ea0400c83dee187e795d1e82c4d
yuwei <yuwei@yuweix.com>
2019-09-24 11:31:30 +0800

    1.0.43

--- assist4j-data/src/main/java/com/assist4j/data/cache/Cache.java
+++ assist4j-data/src/main/java/com/assist4j/data/cache/Cache.java
@@ -11,7 +11,7 @@ import java.util.Set;
  */
 public interface Cache {
 	/**
-	 * 缓存中是否包含key
+	 * 是否包含指定的key
 	 * 找到返回为true，找不到返回为false
 	 * @param key 查找的值
 	 * @return 是否找到
@@ -19,7 +19,7 @@ public interface Cache {
 	boolean contains(String key);
 
 	/**
-	 * 更新缓存中指定key的值
+	 * 更新指定key的值
 	 * @param key 缓存key。
 	 * @param value 缓存的值。
 	 * @param expiredTime 过期时间(s)。
@@ -28,7 +28,7 @@ public interface Cache {
 	<T>boolean put(String key, T value, long expiredTime);
 
 	/**
-	 * 往缓存中存一个键值对，指定过期时间。
+	 * 更新指定key的值，指定过期时间。
 	 * @param key 缓存key。
 	 * @param value 缓存的值。
 	 * @param expiredTime 过期时间。
@@ -37,14 +37,14 @@ public interface Cache {
 	<T>boolean put(String key, T value, Date expiredTime);
 
 	/**
-	 * 取得缓存对象
+	 * 获取指定key的值
 	 * @param key 缓存对象的key
 	 * @return 查询到的缓存的对象
 	 */
 	<T>T get(String key);
 
 	/**
-	 * 删除缓存中对应的key的对象
+	 * 删除指定key
 	 * @param key 缓存对象的key
 	 */
 	void remove(String key);
