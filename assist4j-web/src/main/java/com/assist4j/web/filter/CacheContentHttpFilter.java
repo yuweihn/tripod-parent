b5060bca72bfa1095433196cfbc5f308a935c104
yuwei <yuwei@yuweix.com>
2019-10-08 12:07:07 +0800

    1.1.21

--- assist4j-web/src/main/java/com/assist4j/web/filter/CacheContentHttpFilter.java
+++ assist4j-web/src/main/java/com/assist4j/web/filter/CacheContentHttpFilter.java
@@ -9,7 +9,6 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 
