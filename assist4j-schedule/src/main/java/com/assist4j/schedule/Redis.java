2b809f5c51e28bc9ec3e2051c8637524b8d70979
yuwei <yu.wei@niuhome.com>
2019-10-24 21:37:50 +0800

    1.0.43

--- assist4j-schedule/src/main/java/com/assist4j/schedule/Redis.java
+++ assist4j-schedule/src/main/java/com/assist4j/schedule/Redis.java
@@ -11,5 +11,6 @@ public interface Redis {
 	 * @param expiredTime 单位：秒。
 	 */
 	boolean lock(String key, String owner, long expiredTime);
+	boolean unlock(String key, String owner);
 	String get(String key);
 }
