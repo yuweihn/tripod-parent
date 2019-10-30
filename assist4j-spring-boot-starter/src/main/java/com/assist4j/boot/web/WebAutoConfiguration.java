828a1c3b52096c286f99e82c9bbfd7578f814134
yuwei <yuwei@yuweix.com>
2019-10-30 19:06:11 +0800

    1.1.21

--- assist4j-spring-boot-starter/src/main/java/com/assist4j/boot/web/WebAutoConfiguration.java
+++ assist4j-spring-boot-starter/src/main/java/com/assist4j/boot/web/WebAutoConfiguration.java
@@ -23,33 +23,33 @@ import java.lang.reflect.Constructor;
 @ConditionalOnProperty(name = "assist4j.boot.web.enabled")
 public class WebAutoConfiguration {
 
-    @ConditionalOnBean(name = "messageSource")
-    @ConditionalOnMissingBean
-    @Bean
-    public TextUtil textUtil(@Qualifier("messageSource") MessageSource messageSource) {
-        try {
-            Class<?> clz = Class.forName(TextUtil.class.getName());
-            Constructor<?> constructor = clz.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            TextUtil textUtil = (TextUtil) constructor.newInstance();
-            textUtil.setMessageSource(messageSource);
-            return textUtil;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@ConditionalOnBean(name = "messageSource")
+	@ConditionalOnMissingBean
+	@Bean
+	public TextUtil textUtil(@Qualifier("messageSource") MessageSource messageSource) {
+		try {
+			Class<?> clz = Class.forName(TextUtil.class.getName());
+			Constructor<?> constructor = clz.getDeclaredConstructor();
+			constructor.setAccessible(true);
+			TextUtil textUtil = (TextUtil) constructor.newInstance();
+			textUtil.setMessageSource(messageSource);
+			return textUtil;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @ConditionalOnMissingBean(name = "localeResolver")
-    @Bean(name = "localeResolver")
-    public LocaleResolver localeResolver() {
-        return new SessionLocaleResolver();
-    }
+	@ConditionalOnMissingBean(name = "localeResolver")
+	@Bean(name = "localeResolver")
+	public LocaleResolver localeResolver() {
+		return new SessionLocaleResolver();
+	}
 
-    @ConditionalOnMissingBean(name = "messageSource")
-    @Bean(name = "messageSource")
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("bundles.message");
-        return messageSource;
-    }
+	@ConditionalOnMissingBean(name = "messageSource")
+	@Bean(name = "messageSource")
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("bundles.message");
+		return messageSource;
+	}
 }
