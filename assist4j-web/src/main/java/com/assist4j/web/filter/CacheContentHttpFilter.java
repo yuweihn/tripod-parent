ed22f483cf3f92ac0967c4f48fca01e04a08ce99
yuwei <yuwei@yuweix.com>
2019-11-06 13:23:36 +0800

    1.0.43

--- assist4j-web/src/main/java/com/assist4j/web/filter/CacheContentHttpFilter.java
+++ assist4j-web/src/main/java/com/assist4j/web/filter/CacheContentHttpFilter.java
@@ -1,8 +1,6 @@
 package com.assist4j.web.filter;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
@@ -10,62 +8,73 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 /**
  * @author yuwei
  */
 public class CacheContentHttpFilter extends AbstractFilter<ContentCachingRequestWrapper, ContentCachingResponseWrapper> {
-    private static final Logger log = LoggerFactory.getLogger(CacheContentHttpFilter.class);
-
-    private int contentLimit = 100;
+	private int contentLimit = 100;
 
 
-    public void setContentLimit(int contentLimit) {
-        this.contentLimit = contentLimit;
-    }
+	public void setContentLimit(int contentLimit) {
+		this.contentLimit = contentLimit;
+	}
 
 
 
-    @Override
-    protected ContentCachingRequestWrapper wrap(HttpServletRequest request) {
-        return new ContentCachingRequestWrapper(request);
-    }
+	@Override
+	protected ContentCachingRequestWrapper wrap(HttpServletRequest request) {
+		return new ContentCachingRequestWrapper(request);
+	}
 
-    @Override
-    protected ContentCachingResponseWrapper wrap(HttpServletResponse response) {
-        return new ContentCachingResponseWrapper(response);
-    }
+	@Override
+	protected ContentCachingResponseWrapper wrap(HttpServletResponse response) {
+		return new ContentCachingResponseWrapper(response);
+	}
 
-    @Override
-    protected void logRequest(ContentCachingRequestWrapper request) {
-        super.logRequest(request);
-        printBody(request);
-    }
+	@Override
+	protected Map<String, Object> logRequest(ContentCachingRequestWrapper request) {
+		Map<String, Object> logInfoMap = super.logRequest(request);
+		String bodyInfo = getBodyInfo(request);
+		
+		if (bodyInfo == null || "".equals(bodyInfo)) {
+			return logInfoMap;
+		}
+		
+		if (logInfoMap == null) {
+			logInfoMap = new LinkedHashMap<String, Object>();
+		}
+		logInfoMap.put("requestBody", bodyInfo);
+		return logInfoMap;
+	}
 
-    private void printBody(ContentCachingRequestWrapper request) {
-        byte[] bytes = request.getContentAsByteArray();
-        if (bytes == null || bytes.length <= 0) {
-            return;
-        }
+	private String getBodyInfo(ContentCachingRequestWrapper request) {
+		byte[] bytes = request.getContentAsByteArray();
+		if (bytes == null || bytes.length <= 0) {
+			return null;
+		}
 
-        String content = new String(bytes);
-        if (content == null || "".equals(content)) {
-            return;
-        }
-        try {
-            content = URLDecoder.decode(content, "utf-8");
-        } catch (Exception e) {
-        }
+		String content = new String(bytes);
+		if (content == null || "".equals(content)) {
+			return null;
+		}
+		try {
+			content = URLDecoder.decode(content, "utf-8");
+		} catch (Exception e) {
+			content = null;
+		}
 
-        if (contentLimit > 0 && contentLimit < content.length()) {
-            content = content.substring(0, contentLimit) + "......";
-        }
-        log.info("RequestBody: {}", content);
-    }
+		if (contentLimit > 0 && contentLimit < content.length()) {
+			content = content.substring(0, contentLimit) + "......";
+		}
+		return content;
+	}
 
-    @Override
-    protected void afterFilter(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) throws IOException {
-        response.copyBodyToResponse();
-    }
+	@Override
+	protected void afterFilter(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) throws IOException {
+		response.copyBodyToResponse();
+	}
 }
