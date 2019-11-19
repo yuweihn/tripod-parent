e763c6601d5614d17015694aad7eb3e02b1e79d8
yuwei <yuwei@yuweix.com>
2019-11-19 14:37:54 +0800

    1.1.21

--- assist4j-data/src/main/java/com/assist4j/data/cache/redis/RedisCache.java
+++ assist4j-data/src/main/java/com/assist4j/data/cache/redis/RedisCache.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import com.assist4j.data.cache.Cache;
 import com.assist4j.data.cache.MessageCache;
-import com.assist4j.data.cache.DistLock;
+import com.assist4j.data.cache.Lock;
 
 
 /**
  * @author yuwei
  */
-public interface RedisCache extends Cache, MessageCache, DistLock {
+public interface RedisCache extends Cache, MessageCache, Lock {
 	String execute(String script, List<String> keyList, List<String> argList);
 }
