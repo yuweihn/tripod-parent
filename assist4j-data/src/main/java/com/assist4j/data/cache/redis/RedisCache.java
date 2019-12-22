76e0d07007fc7ca6ca624f3ae06aa7744c6b395d
yuwei <yu.wei@niuhome.com>
2019-12-22 22:00:19 +0800

    1.1.22

--- assist4j-data/src/main/java/com/assist4j/data/cache/redis/RedisCache.java
+++ assist4j-data/src/main/java/com/assist4j/data/cache/redis/RedisCache.java
@@ -4,13 +4,37 @@ package com.assist4j.data.cache.redis;
 import java.util.List;
 
 import com.assist4j.data.cache.Cache;
-import com.assist4j.data.cache.MessageCache;
-import com.assist4j.data.cache.Lock;
+import com.assist4j.data.cache.MessageHandler;
 
 
 /**
  * @author yuwei
  */
-public interface RedisCache extends Cache, MessageCache, Lock {
+public interface RedisCache extends Cache {
+	/**
+	 * 发布消息
+	 * @param channel
+	 * @param message
+	 */
+	void publish(String channel, String message);
+
+	/**
+	 * 订阅消息
+	 * @param channel
+	 * @param handler
+	 */
+	void subscribe(String channel, MessageHandler handler);
+
+	<T>boolean lock(String key, T owner, long timeout);
+	/**
+	 * @param key
+	 * @param owner
+	 * @param timeout 单位：秒。
+	 * @param reentrant   是否可重入
+	 * @return
+	 */
+	<T>boolean lock(String key, T owner, long timeout, boolean reentrant);
+	<T>boolean unlock(String key, T owner);
+
 	String execute(String script, List<String> keyList, List<String> argList);
 }
