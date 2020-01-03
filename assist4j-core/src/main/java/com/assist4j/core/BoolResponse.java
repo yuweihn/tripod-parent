bff8de4da13f0159efd76aec2e625c6aa8690490
yuwei <yuwei@yuweix.com>
2020-01-03 17:52:59 +0800

    1.1.22

---
+++ assist4j-core/src/main/java/com/assist4j/core/BoolResponse.java
@@ -0,0 +1,10 @@
+package com.assist4j.core;
+
+
+
+/**
+ * @author yuwei
+ */
+public class BoolResponse<D> extends Response<Boolean, D> {
+
+}
---
+++ assist4j-core/src/main/java/com/assist4j/core/StringResponse.java
@@ -0,0 +1,10 @@
+package com.assist4j.core;
+
+
+
+/**
+ * @author yuwei
+ */
+public class StringResponse<D> extends Response<String, D> {
+
+}
