49f2102b175d0ab9f7c46bf3b148e5d18d7a32e9
yuwei <yu.wei@niuhome.com>
2020-03-08 00:09:22 +0800

    1.0.45

--- assist4j-data/src/main/java/com/assist4j/data/cache/redis/jedis/JedisClusterCache.java
+++ assist4j-data/src/main/java/com/assist4j/data/cache/redis/jedis/JedisClusterCache.java
@@ -42,13 +42,18 @@ public class JedisClusterCache extends AbstractCache implements RedisCache {
 	}
 
 	@Override
-	public void subscribe(String channel, final MessageHandler handler) {
-		jedisCluster.subscribe(new JedisPubSub() {
+	public void subscribe(final String channel, final MessageHandler handler) {
+		new Thread() {
 			@Override
-			public void onMessage(String channel, String message) {
-				handler.handle(channel, message);
+			public void run() {
+				jedisCluster.subscribe(new JedisPubSub() {
+					@Override
+					public void onMessage(String channel, String message) {
+						handler.handle(channel, message);
+					}
+				}, channel);
 			}
-		}, channel);
+		}.start();
 	}
 
 	@Override
