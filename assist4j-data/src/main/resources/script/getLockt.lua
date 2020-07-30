a8cee9b03318e9845cb4484dc85232e998d7ef5a
yuwei <yuwei@yuweix.com>
2020-07-30 19:50:57 +0800

    1.1.28

--- assist4j-data/src/main/resources/script/getLockt.lua
+++ assist4j-data/src/main/resources/script/getLockt.lua
@@ -1,13 +1,10 @@
 if redis.call('get', KEYS[1]) == ARGV[1] then
-    if redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2], 'XX') == "OK" then
-        return ARGV[1];
-    else
-        return nil;
-    end
+    redis.call('expire', KEYS[1], ARGV[2]);
+    return ARGV[1];
+end
+
+if redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2], 'NX') == "OK" then
+    return ARGV[1];
 else
-    if redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2], 'NX') == "OK" then
-        return ARGV[1];
-    else
-        return redis.call('get', KEYS[1]);
-    end
+    return redis.call('get', KEYS[1]);
 end
