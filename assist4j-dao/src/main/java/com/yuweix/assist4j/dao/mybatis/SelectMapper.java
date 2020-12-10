748fbfbabfca5a3aa5a6e479edc0b4b2adcd29a3
yuwei <fisher.yu@agilenaas.com>
2020-12-10 16:20:22 +0800

    1.1.0

--- assist4j-dao/src/main/java/com/yuweix/assist4j/dao/mybatis/SelectMapper.java
+++ assist4j-dao/src/main/java/com/yuweix/assist4j/dao/mybatis/SelectMapper.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.annotations.SelectProvider;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -18,29 +17,6 @@ import java.util.Map;
 public interface SelectMapper<T extends Serializable, PK extends Serializable> {
 	@SelectProvider(type = SelectSqlProvider.class, method = "selectOneById")
 	T selectOneById(@Param("id") PK id, @Param("clz") Class<T> clz);
-	
-	/**
-	 * @param where                   where中的key为表的字段，不是对象的属性
-	 * @param clazz
-	 * @return
-	 */
-	@SelectProvider(type = SelectSqlProvider.class, method = "selectCount")
-	int selectCount(@Param("where") Map<String, Object> where, @Param("clazz") Class<T> clazz);
-
-	/**
-	 * @param where                   where中的key为表的字段，不是对象的属性
-	 * @param orderBy
-	 * @param clazz
-	 * @return
-	 */
-	@SelectProvider(type = SelectSqlProvider.class, method = "selectList")
-	List<T> selectList(@Param("where") Map<String, Object> where, @Param("orderBy") OrderBy orderBy, @Param("clazz") Class<T> clazz);
-
-	@SelectProvider(type = SelectSqlProvider.class, method = "selectList")
-	List<T> selectPageList(@Param("where") Map<String, Object> where, @Param("pageNo") int pageNo
-			, @Param("pageSize") int pageSize, @Param("orderBy") OrderBy orderBy, @Param("clazz") Class<T> clazz);
-
-
 
 	@SelectProvider(type = SelectSqlProvider.class, method = "findCount")
 	int findCount(@Param("criteria") Criteria criteria, @Param("clazz") Class<T> clazz);
