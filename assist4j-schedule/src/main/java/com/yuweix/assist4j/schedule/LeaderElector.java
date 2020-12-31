4c6bc599c01338fef7cbf36e468f9905abb69c69
yuwei <fisher.yu@agilenaas.com>
2020-12-31 17:43:28 +0800

    1.1.5

--- assist4j-schedule/src/main/java/com/yuweix/assist4j/schedule/LeaderElector.java
+++ assist4j-schedule/src/main/java/com/yuweix/assist4j/schedule/LeaderElector.java
@@ -4,10 +4,11 @@ package com.yuweix.assist4j.schedule;
 
 
 /**
+ * Leader选择器
  * @author yuwei
  */
 public interface LeaderElector {
 	String acquire(String lock);
 	void release(String lock);
-	String getLocalNode(String lock);
+	String getLocalNode();
 }
