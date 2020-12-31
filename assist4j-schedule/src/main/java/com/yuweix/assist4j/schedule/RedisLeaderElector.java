0d7d5120b8c513f2c9042bfc8e243947c7978251
yuwei <fisher.yu@agilenaas.com>
2020-12-31 17:45:07 +0800

    1.1.5

--- assist4j-schedule/src/main/java/com/yuweix/assist4j/schedule/RedisLeaderElector.java
+++ assist4j-schedule/src/main/java/com/yuweix/assist4j/schedule/RedisLeaderElector.java
@@ -7,7 +7,7 @@ package com.yuweix.assist4j.schedule;
  * @author yuwei
  */
 public class RedisLeaderElector extends AbstractLeaderElector {
-	private static final String CACHE_LEADER_KEY_PRE = "cache.schedule.leader.";
+	private static final String CACHE_LEADER_KEY_PRE = "cache%s.schedule.leader.";
 
 	private Redis redis;
 
@@ -16,23 +16,31 @@ public class RedisLeaderElector extends AbstractLeaderElector {
 	 */
 	private int timeout;
 
+	private String prjNo;
+
 
 	public RedisLeaderElector(Redis redis, int timeout) {
+		this(redis, timeout, null);
+	}
+	public RedisLeaderElector(Redis redis, int timeout, String prjNo) {
 		super();
 		this.redis = redis;
 		this.timeout = timeout;
+		this.prjNo = prjNo;
 	}
 
 	@Override
 	public String acquire(String lock) {
-		String key = CACHE_LEADER_KEY_PRE + lock;
-		return redis.lock(lock, getLocalNode(key), timeout / 1000);
+		String key = String.format(CACHE_LEADER_KEY_PRE
+				, this.prjNo == null || "".equals(this.prjNo.trim()) ? "" : "." + this.prjNo.trim()) + lock;
+		return redis.lock(key, getLocalNode(), timeout / 1000);
 	}
 
 	@Override
 	public void release(String lock) {
-		String key = CACHE_LEADER_KEY_PRE + lock;
-		redis.unlock(lock, getLocalNode(key));
+		String key = String.format(CACHE_LEADER_KEY_PRE
+				, this.prjNo == null || "".equals(this.prjNo.trim()) ? "" : "." + this.prjNo.trim()) + lock;
+		redis.unlock(key, getLocalNode());
 	}
 
 	@Override
