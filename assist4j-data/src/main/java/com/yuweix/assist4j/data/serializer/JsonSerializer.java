1624fefb3b13a44a8b0fb6a3a205b37f684dc71a
yuwei <fisher.yu@agilenaas.com>
2021-02-25 18:00:36 +0800

    1.1.7

---
+++ assist4j-data/src/main/java/com/yuweix/assist4j/data/serializer/JsonSerializer.java
@@ -0,0 +1,39 @@
+package com.yuweix.assist4j.data.serializer;
+
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * 序列化工具
+ * @author yuwei
+ */
+public class JsonSerializer implements Serializer {
+	private static final Logger log = LoggerFactory.getLogger(JsonSerializer.class);
+
+
+	/**
+	 * 序列化
+	 */
+	public <T>String serialize(T t) {
+		if (t == null) {
+			return null;
+		}
+
+		return JSONObject.toJSONString(new JsonText(t.getClass().getName(), t));
+	}
+
+	/**
+	 * 反序列化
+	 */
+	public <T>T deserialize(String str) {
+		if (str == null) {
+			return null;
+		}
+
+		JsonText jsonText = JSONObject.parseObject(str, JsonText.class);
+		return (T) jsonText.getText();
+	}
+}
