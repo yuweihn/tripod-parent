99deb17cb863c8260bc28c9404a0094ab5f62d9c
yuwei <fisher.yu@agilenaas.com>
2021-08-23 11:02:50 +0800

    1.1.13

--- assist4j-datasecure/src/main/resources/script/getLock.lua
+++
@@ -1,9 +0,0 @@
-if ARGV[1] == 'true' then
-    if redis.call('get', KEYS[1]) == ARGV[2] then
-        return redis.call('set', KEYS[1], ARGV[2], 'EX', ARGV[3], 'XX')
-    else
-        return redis.call('set', KEYS[1], ARGV[2], 'EX', ARGV[3], 'NX')
-    end
-else
-    return redis.call('set', KEYS[1], ARGV[2], 'EX', ARGV[3], 'NX')
-end
