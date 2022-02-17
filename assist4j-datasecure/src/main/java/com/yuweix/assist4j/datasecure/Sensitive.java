3d4cb5156cd65b837bc10e6cfcbbfb7317ccb51a
yuwei <fisher.yu@agilenaas.com>
2022-02-17 14:35:34 +0800

    1.1.18

--- assist4j-datasecure/src/main/java/com/yuweix/assist4j/datasecure/Sensitive.java
+++ assist4j-datasecure/src/main/java/com/yuweix/assist4j/datasecure/Sensitive.java
@@ -6,10 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 
 
 /**
+ * 基于正则表达式的脱敏注解
  * eg.
- * MOBILE_PHONE("mobile_phone", "11位手机号", "^(\\d{3})\\d{4}(\\d{4})$", "$1****$2"),//$1第一个括号的内容,$2第二个括号里面的内容
- * ID_CARD("id_card", "16或者18身份证号", "^(\\d{4})\\d{8,10}(\\d{4})$", "$1****$2"),
- * BANK_CARD_NO("bankCardNo", "银行卡号", "^(\\d{4})\\d*(\\d{4})$", "$1****$2")
+ * 11位手机号            ^(\\d{3})\\d{4}(\\d{4})$, $1****$2
+ * 16或者18身份证号       ^(\\d{4})\\d{8,10}(\\d{4})$, $1****$2
+ * 银行卡号              ^(\\d{4})\\d*(\\d{4}), $1****$2
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Sensitive {
