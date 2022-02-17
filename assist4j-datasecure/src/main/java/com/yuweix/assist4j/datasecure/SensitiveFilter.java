b959967b6d38680ffbdcfe111d2af08d537d2d68
yuwei <fisher.yu@agilenaas.com>
2022-02-17 15:23:01 +0800

    1.1.18

--- assist4j-datasecure/src/main/java/com/yuweix/assist4j/datasecure/SensitiveFilter.java
+++ assist4j-datasecure/src/main/java/com/yuweix/assist4j/datasecure/SensitiveFilter.java
@@ -2,31 +2,11 @@ package com.yuweix.assist4j.datasecure;
 
 
 import com.alibaba.fastjson.serializer.ValueFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Field;
 
 
 public class SensitiveFilter implements ValueFilter {
-    private static final Logger log = LoggerFactory.getLogger(SensitiveFilter.class);
-
 	@Override
     public Object process(Object object, String name, Object value) {
-        if (!(value instanceof String) || ((String) value).length() == 0) {
-            return value;
-        }
-        try {
-            Field field = object.getClass().getDeclaredField(name);
-            Sensitive sensitive;
-            if (String.class != field.getType() || (sensitive = field.getAnnotation(Sensitive.class)) == null) {
-                return value;
-            }
-            
-            return SensitiveUtil.shield((String) value, sensitive);
-        } catch (NoSuchFieldException e) {
-            log.warn("The class {} has no field {}", object.getClass(), name);
-        }
-        return value;
+        return SensitiveUtil.shield(object, name, value);
     }
 }
--- assist4j-datasecure/src/main/java/com/yuweix/assist4j/datasecure/SensitiveUtil.java
+++ assist4j-datasecure/src/main/java/com/yuweix/assist4j/datasecure/SensitiveUtil.java
@@ -1,29 +1,42 @@
 package com.yuweix.assist4j.datasecure;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
 
 
 /**
  * 脱敏工具类
  */
 public class SensitiveUtil {
-    /**
-     * 按敏感信息格式化转换字符串信息
-     * @return
-     */
-    public static String shield(String info, Sensitive sensitive) {
-        if (sensitive == null || info == null || "".equals(info)) {
-            return info;
+    private static final Logger log = LoggerFactory.getLogger(SensitiveUtil.class);
+
+    public static Object shield(Object object, String fieldName, Object val) {
+        if (!(val instanceof String) || "".equals(val)) {
+            return val;
         }
-        return shield(info, sensitive.regex(), sensitive.replacement());
+        try {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            Sensitive sensitive;
+            if (String.class != field.getType() || (sensitive = field.getAnnotation(Sensitive.class)) == null) {
+                return val;
+            }
+
+            return shield((String) val, sensitive.regex(), sensitive.replacement());
+        } catch (NoSuchFieldException e) {
+            log.warn("The class {} has no field {}", object.getClass(), fieldName);
+        }
+        return val;
     }
 
-    public static String shield(String info, String regex, String replacement) {
-        if (info == null || "".equals(info)
+    public static String shield(String val, String regex, String replacement) {
+        if (val == null || "".equals(val)
                 || regex == null || "".equals(regex.trim())
                 || replacement == null || "".equals(replacement.trim())) {
-            return info;
+            return val;
         }
-        return info.replaceAll(regex.trim(), replacement.trim());
+        return val.replaceAll(regex.trim(), replacement.trim());
     }
 }
