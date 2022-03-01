2a27027b116b3df4dfb47fa00af6d624a22c5eb3
yuwei <fisher.yu@agilenaas.com>
2022-03-01 10:31:56 +0800

    1.1.19

--- assist4j-data/src/main/java/com/yuweix/assist4j/data/datasecure/FastjsonSensitiveFilter.java
+++ assist4j-data/src/main/java/com/yuweix/assist4j/data/datasecure/FastjsonSensitiveFilter.java
@@ -4,6 +4,9 @@ package com.yuweix.assist4j.data.datasecure;
 import com.alibaba.fastjson.serializer.ValueFilter;
 
 
+/**
+ * @author yuwei
+ */
 public class FastjsonSensitiveFilter implements ValueFilter {
 	@Override
     public Object process(Object object, String name, Object value) {
