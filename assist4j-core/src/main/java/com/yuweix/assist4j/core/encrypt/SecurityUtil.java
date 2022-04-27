8c5a92fffd473d57badbd64298ad23701c1ffe97
yuwei <yuwei@yuweix.com>
2022-04-27 23:49:17 +0800

    1.1.19

--- assist4j-core/src/main/java/com/yuweix/assist4j/core/encrypt/SecurityUtil.java
+++ assist4j-core/src/main/java/com/yuweix/assist4j/core/encrypt/SecurityUtil.java
@@ -4,7 +4,6 @@ package com.yuweix.assist4j.core.encrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Objects;
@@ -20,21 +19,21 @@ import javax.crypto.spec.SecretKeySpec;
 public abstract class SecurityUtil {
 	private static final Logger log = LoggerFactory.getLogger(SecurityUtil.class);
 	private static final String SECURITY_KEY = "sfdfyu8**((^$$$SDSDhHJlSDDsdsvcx234ex,,,cjv.xckv...";
-	private static final char HEX_DIGIT[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	private static final char[] HEX_DIGIT = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 	private static final String UTF_8 = "utf-8";
 
 
-	public static final String getMd5(String str) {
+	public static String getMd5(String str) {
 		return getMd5(str, UTF_8);
 	}
-	public static final String getMd5(String str, String charset) {
+	public static String getMd5(String str, String charset) {
 		return getSecurityByAlgor(Algor.MD5.getCode(), str, charset);
 	}
 
-	public static final String getSha1(String str) {
+	public static String getSha1(String str) {
 		return getSha1(str, UTF_8);
 	}
-	public static final String getSha1(String str, String charset) {
+	public static String getSha1(String str, String charset) {
 		return getSecurityByAlgor(Algor.SHA1.getCode(), str, charset);
 	}
 
@@ -45,10 +44,9 @@ public abstract class SecurityUtil {
 			mdTemp.update(tmp);
 			byte[] md = mdTemp.digest();
 			int j = md.length;
-			char arr[] = new char[j << 1];
+			char[] arr = new char[j << 1];
 			int k = 0;
-			for (int i = 0; i < j; i++) {
-				byte byte0 = md[i];
+			for (byte byte0 : md) {
 				arr[k++] = HEX_DIGIT[byte0 >>> 4 & 0xf];
 				arr[k++] = HEX_DIGIT[byte0 & 0xf];
 			}
@@ -109,14 +107,14 @@ public abstract class SecurityUtil {
 		try {
 			Cipher decrypt = Cipher.getInstance(Algor.DES.getCode());
 			decrypt.init(2, buildDESKey(getMd5(SECURITY_KEY)));
-			return new String(decrypt.doFinal(hexStrToBytes(word)), StandardCharsets.UTF_8);
+			return new String(decrypt.doFinal(Objects.requireNonNull(hexStrToBytes(word))), StandardCharsets.UTF_8);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 
-	private static SecretKey buildDESKey(String value) throws UnsupportedEncodingException {
+	private static SecretKey buildDESKey(String value) {
 		byte[] bval = value.getBytes(StandardCharsets.UTF_8);
 		byte[] bt = new byte[8];
 		for (int i = 0; (i < bt.length) && (i < bval.length); i++) {
@@ -150,8 +148,8 @@ public abstract class SecurityUtil {
 	 */
 	public static String bytesToHexStr(byte[] value) {
 		StringBuilder builder = new StringBuilder("");
-		for (int i = 0; i < value.length; i++) {
-			String hex = Integer.toHexString(value[i] & 0xFF);
+		for (byte b : value) {
+			String hex = Integer.toHexString(b & 0xFF);
 			if (hex.length() == 1) {
 				hex = '0' + hex;
 			}
