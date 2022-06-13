08f56c7de8db637def7d1592d5e756e6d49daade
yuwei <fisher.yu@agilenaas.com>
2022-06-13 12:04:43 +0800

    1.1.21

--- assist4j-spring-boot-starter/src/main/java/com/yuweix/assist4j/boot/core/JsonAutoConfiguration.java
+++ assist4j-spring-boot-starter/src/main/java/com/yuweix/assist4j/boot/core/JsonAutoConfiguration.java
@@ -21,15 +21,25 @@ import java.lang.reflect.Constructor;
 public class JsonAutoConfiguration {
 	@ConditionalOnMissingBean(Json.class)
 	@Bean
-	public Json json(@Value("${assist4j.boot.json.accept:}") String accept
-			, @Value("${assist4j.boot.json.deny:}") String deny) {
+	public Json json(@Value("${assist4j.boot.json.accept:}") String accepts
+			, @Value("${assist4j.boot.json.deny:}") String denies) {
 		Json json = new Fastjson();
 		json.addAccept("com.yuweix.assist4j.session.SessionAttribute");
-		if (accept != null && !"".equals(accept.trim())) {
-			json.addAccept(accept.trim());
+		if (accepts != null && !"".equals(accepts.trim())) {
+			String[] arr = accepts.trim().split(",");
+			for (String accept: arr) {
+				if (accept != null && !"".equals(accept.trim())) {
+					json.addAccept(accept.trim());
+				}
+			}
 		}
-		if (deny != null && !"".equals(deny.trim())) {
-			json.addDeny(deny.trim());
+		if (denies != null && !"".equals(denies.trim())) {
+			String[] arr = denies.trim().split(",");
+			for (String deny: arr) {
+				if (deny != null && !"".equals(deny.trim())) {
+					json.addDeny(deny.trim());
+				}
+			}
 		}
 		return json;
 	}
