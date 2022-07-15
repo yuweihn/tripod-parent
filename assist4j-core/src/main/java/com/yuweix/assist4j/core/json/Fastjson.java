53746c64f9e525b0cf0c3ec58d9306bd50dfe3a1
yuwei <fisher.yu@agilenaas.com>
2022-07-15 17:38:20 +0800

    1.1.23

--- assist4j-core/src/main/java/com/yuweix/assist4j/core/json/Fastjson.java
+++ assist4j-core/src/main/java/com/yuweix/assist4j/core/json/Fastjson.java
@@ -2,15 +2,23 @@ package com.yuweix.assist4j.core.json;
 
 
 import com.alibaba.fastjson2.*;
+import com.alibaba.fastjson2.filter.Filter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 /**
  * @author yuwei
  */
 public class Fastjson implements Json {
+	private final List<String> autoTypes = new LinkedList<>();
+	private Filter autoTypeFilter;
+
 	@Override
 	public void addAccept(String name) {
-		JSONFactory.getDefaultObjectReaderProvider().addAutoTypeAccept(name);
+		autoTypes.add(name);
+		autoTypeFilter = JSONReader.autoTypeFilter(autoTypes.toArray(new String[0]));
 	}
 
 	@Override
@@ -26,7 +34,11 @@ public class Fastjson implements Json {
 		if (str == null) {
 			return null;
 		}
-		return JSON.parseObject(str, new TypeReference<T>() {}, JSONReader.Feature.SupportAutoType);
+		if (autoTypeFilter == null) {
+			return JSON.parseObject(str, new TypeReference<T>() {});
+		} else {
+			return JSON.parseObject(str, new TypeReference<T>() {}, autoTypeFilter);
+		}
 	}
 
 	@Override
