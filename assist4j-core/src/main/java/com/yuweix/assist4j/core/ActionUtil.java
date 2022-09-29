1a2c1da0108e4c311dbd895e93cfd74e37596ec6
yuwei <fisher.yu@agilenaas.com>
2022-09-29 12:05:20 +0800

    1.1.26

--- assist4j-core/src/main/java/com/yuweix/assist4j/core/ActionUtil.java
+++ assist4j-core/src/main/java/com/yuweix/assist4j/core/ActionUtil.java
@@ -24,15 +24,15 @@ public abstract class ActionUtil {
 	 */
 	public static String getRequestIP() {
 		HttpServletRequest request = getRequest();
-		String ip = request.getHeader("X-Real-IP");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("x-forwarded-for");
-		}
+		String ip = request.getHeader("x-forwarded-for");
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("X-Real-IP");
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
