f58e2c17981a9fd26b18e13aec9a1b4b38431450
yuwei <fisher.yu@agilenaas.com>
2022-11-11 19:09:17 +0800

    1.1.27

--- assist4j-sequence/src/main/java/com/yuweix/assist4j/sequence/springboot/SequenceConf.java
+++ assist4j-sequence/src/main/java/com/yuweix/assist4j/sequence/springboot/SequenceConf.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -58,7 +59,12 @@ public class SequenceConf {
 
 	@ConditionalOnMissingBean(SequenceBeanFactory.class)
 	@Bean(name = "sequenceBeanFactory")
-	public SequenceBeanFactory sequenceBeanFactory() {
-		return new SequenceBeanFactory(DefaultSequence.class);
+	public SequenceBeanFactory sequenceBeanFactory(Environment env) {
+		String sequenceClz = env.getProperty("assist4j.sequence.clz");
+		if (sequenceClz != null && !"".equals(sequenceClz)) {
+			return new SequenceBeanFactory(sequenceClz);
+		} else {
+			return new SequenceBeanFactory(DefaultSequence.class);
+		}
 	}
 }
