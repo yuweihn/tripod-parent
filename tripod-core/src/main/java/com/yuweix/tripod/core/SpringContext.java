b081a9bdd16999a827eb12081805a7b2032dbb79
yuwei <fisher.yu@agilenaas.com>
2022-11-16 16:28:48 +0800

    1.0.1

--- tripod-core/src/main/java/com/yuweix/tripod/core/SpringContext.java
+++ tripod-core/src/main/java/com/yuweix/tripod/core/SpringContext.java
@@ -133,13 +133,13 @@ public class SpringContext implements ApplicationContextAware {
 	}
 	
 	
-	private static class Property {
+	public static class Property {
 		private String propertyName;
 		private Object value;
 		private byte type;
 		
-		static final byte TYPE_VALUE = 0;
-		static final byte TYPE_REFERENCE = 1;
+		public static final byte TYPE_VALUE = 0;
+		public static final byte TYPE_REFERENCE = 1;
 		
 		public Property() {
 
