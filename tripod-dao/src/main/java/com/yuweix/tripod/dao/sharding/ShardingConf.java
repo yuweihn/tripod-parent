ed2b9d023c0a111683692f44215733e419c67dcc
yuwei <yuwei@yuweix.com>
2022-11-19 20:05:26 +0800

    1.0.2

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/ShardingConf.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/ShardingConf.java
@@ -12,14 +12,14 @@ import java.util.Map;
  * @author yuwei
  */
 public class ShardingConf {
-	interface TableHolder {
+	interface H {
 		Map<String, TableConfig> getTables();
 	}
 
 	@Bean
 	@ConfigurationProperties(prefix = "tripod.sharding", ignoreUnknownFields = true)
-	public TableHolder tableHolder() {
-		return new TableHolder() {
+	public H shardingTableHolder() {
+		return new H() {
 			private Map<String, TableConfig> map = new HashMap<>();
 
 			@Override
@@ -30,8 +30,8 @@ public class ShardingConf {
 	}
 
 	@Bean(name = "shardingTableConf")
-	public Map<String, TableConfig> shardingTableConf(TableHolder tableHolder) {
-		Map<String, TableConfig> map = tableHolder.getTables();
+	public Map<String, TableConfig> shardingTableConf(H shardingTableHolder) {
+		Map<String, TableConfig> map = shardingTableHolder.getTables();
 		Constant.initTableConf(map);
 		return map;
 	}
