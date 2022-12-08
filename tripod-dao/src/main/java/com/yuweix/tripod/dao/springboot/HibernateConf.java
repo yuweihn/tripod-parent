88dc7203387b79e330f4f88099922f5c223c0245
yuwei <fisher.yu@agilenaas.com>
2022-12-08 11:01:58 +0800

    1.0.4

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/springboot/HibernateConf.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/springboot/HibernateConf.java
@@ -34,7 +34,7 @@ public class HibernateConf {
 
 	@ConditionalOnMissingBean(name = "dataSource")
 	@Bean(name = "dataSource", initMethod = "init", destroyMethod = "close")
-	public DataSource druidDataSourceMaster(@Value("${tripod.datasource.default.driver-class}") String driverClassName
+	public DataSource defaultDataSource(@Value("${tripod.datasource.default.driver-class}") String driverClassName
 			, @Value("${tripod.datasource.default.url}") String url
 			, @Value("${tripod.datasource.default.user-name}") String userName
 			, @Value("${tripod.datasource.default.password}") String password
