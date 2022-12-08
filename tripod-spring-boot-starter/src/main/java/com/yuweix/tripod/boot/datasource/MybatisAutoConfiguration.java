70024e09a2d116b42fff969cfdbd4582cab666a1
yuwei <fisher.yu@agilenaas.com>
2022-12-08 10:21:01 +0800

    1.0.4

--- tripod-spring-boot-starter/src/main/java/com/yuweix/tripod/boot/datasource/MybatisAutoConfiguration.java
+++ tripod-spring-boot-starter/src/main/java/com/yuweix/tripod/boot/datasource/MybatisAutoConfiguration.java
@@ -45,27 +45,27 @@ public class MybatisAutoConfiguration {
 
 	@ConditionalOnMissingBean(name = "dataSource")
 	@Bean(name = "dataSource", initMethod = "init", destroyMethod = "close")
-	public DataSource druidDataSourceMaster(@Value("${tripod.datasource.driver-class}") String driverClassName
-			, @Value("${tripod.datasource.url}") String url
-			, @Value("${tripod.datasource.user-name}") String userName
-			, @Value("${tripod.datasource.password}") String password
-			, @Value("${tripod.datasource.default-read-only:false}") boolean defaultReadOnly
-			, @Value("${tripod.datasource.filters:stat}") String filters
-			, @Value("${tripod.datasource.max-active:2}") int maxActive
-			, @Value("${tripod.datasource.initial-size:1}") int initialSize
-			, @Value("${tripod.datasource.max-wait-mills:60000}") long maxWaitMillis
-			, @Value("${tripod.datasource.remove-abandoned:false}") boolean removeAbandoned
-			, @Value("${tripod.datasource.remove-abandoned-timeout:1800}") int removeAbandonedTimeout
-			, @Value("${tripod.datasource.min-idle:1}") int minIdle
-			, @Value("${tripod.datasource.time-between-eviction-runs-millis:60000}") long timeBetweenEvictionRunsMillis
-			, @Value("${tripod.datasource.min-evictable-idle-time-millis:300000}") long minEvictableIdleTimeMillis
-			, @Value("${tripod.datasource.validation-query:select 'x'}") String validationQuery
-			, @Value("${tripod.datasource.test-while-idle:true}") boolean testWhileIdle
-			, @Value("${tripod.datasource.test-on-borrow:false}") boolean testOnBorrow
-			, @Value("${tripod.datasource.test-on-return:false}") boolean testOnReturn
-			, @Value("${tripod.datasource.pool-prepared-statements:true}") boolean poolPreparedStatements
-			, @Value("${tripod.datasource.max-pool-prepared-statement-per-connection-size:50}") int maxPoolPreparedStatementPerConnectionSize
-			, @Value("${tripod.datasource.max-open-prepared-statements:100}") int maxOpenPreparedStatements) throws SQLException {
+	public DataSource druidDataSourceMaster(@Value("${tripod.datasource.default.driver-class}") String driverClassName
+			, @Value("${tripod.datasource.default.url}") String url
+			, @Value("${tripod.datasource.default.user-name}") String userName
+			, @Value("${tripod.datasource.default.password}") String password
+			, @Value("${tripod.datasource.default.default-read-only:false}") boolean defaultReadOnly
+			, @Value("${tripod.datasource.default.filters:stat}") String filters
+			, @Value("${tripod.datasource.default.max-active:2}") int maxActive
+			, @Value("${tripod.datasource.default.initial-size:1}") int initialSize
+			, @Value("${tripod.datasource.default.max-wait-mills:60000}") long maxWaitMillis
+			, @Value("${tripod.datasource.default.remove-abandoned:false}") boolean removeAbandoned
+			, @Value("${tripod.datasource.default.remove-abandoned-timeout:1800}") int removeAbandonedTimeout
+			, @Value("${tripod.datasource.default.min-idle:1}") int minIdle
+			, @Value("${tripod.datasource.default.time-between-eviction-runs-millis:60000}") long timeBetweenEvictionRunsMillis
+			, @Value("${tripod.datasource.default.min-evictable-idle-time-millis:300000}") long minEvictableIdleTimeMillis
+			, @Value("${tripod.datasource.default.validation-query:select 'x'}") String validationQuery
+			, @Value("${tripod.datasource.default.test-while-idle:true}") boolean testWhileIdle
+			, @Value("${tripod.datasource.default.test-on-borrow:false}") boolean testOnBorrow
+			, @Value("${tripod.datasource.default.test-on-return:false}") boolean testOnReturn
+			, @Value("${tripod.datasource.default.pool-prepared-statements:true}") boolean poolPreparedStatements
+			, @Value("${tripod.datasource.default.max-pool-prepared-statement-per-connection-size:50}") int maxPoolPreparedStatementPerConnectionSize
+			, @Value("${tripod.datasource.default.max-open-prepared-statements:100}") int maxOpenPreparedStatements) throws SQLException {
 		DruidDataSource dataSource = new DruidDataSource();
 		dataSource.setDriverClassName(driverClassName);
 		dataSource.setUrl(url);
