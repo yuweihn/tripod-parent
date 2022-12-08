6c7072d92d4bab4b5a05f47c79f67a70c7bffb03
yuwei <fisher.yu@agilenaas.com>
2022-12-08 10:36:10 +0800

    1.0.4

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/springboot/HibernateConf.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/springboot/HibernateConf.java
@@ -1,6 +1,7 @@
 package com.yuweix.tripod.dao.springboot;
 
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.yuweix.tripod.dao.hibernate.DynamicTableInterceptor;
 import com.yuweix.tripod.dao.hibernate.ShardAspect;
 import com.yuweix.tripod.dao.sharding.ShardingContext;
@@ -18,6 +19,7 @@ import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.Properties;
 
 
@@ -29,6 +31,54 @@ public class HibernateConf {
 	@javax.annotation.Resource
 	private ApplicationContext applicationContext;
 
+
+	@ConditionalOnMissingBean(name = "dataSource")
+	@Bean(name = "dataSource", initMethod = "init", destroyMethod = "close")
+	public DataSource druidDataSourceMaster(@Value("${tripod.datasource.default.driver-class}") String driverClassName
+			, @Value("${tripod.datasource.default.url}") String url
+			, @Value("${tripod.datasource.default.user-name}") String userName
+			, @Value("${tripod.datasource.default.password}") String password
+			, @Value("${tripod.datasource.default.default-read-only:false}") boolean defaultReadOnly
+			, @Value("${tripod.datasource.default.filters:stat}") String filters
+			, @Value("${tripod.datasource.default.max-active:2}") int maxActive
+			, @Value("${tripod.datasource.default.initial-size:1}") int initialSize
+			, @Value("${tripod.datasource.default.max-wait-mills:60000}") long maxWaitMillis
+			, @Value("${tripod.datasource.default.remove-abandoned:false}") boolean removeAbandoned
+			, @Value("${tripod.datasource.default.remove-abandoned-timeout:1800}") int removeAbandonedTimeout
+			, @Value("${tripod.datasource.default.min-idle:1}") int minIdle
+			, @Value("${tripod.datasource.default.time-between-eviction-runs-millis:60000}") long timeBetweenEvictionRunsMillis
+			, @Value("${tripod.datasource.default.min-evictable-idle-time-millis:300000}") long minEvictableIdleTimeMillis
+			, @Value("${tripod.datasource.default.validation-query:select 'x'}") String validationQuery
+			, @Value("${tripod.datasource.default.test-while-idle:true}") boolean testWhileIdle
+			, @Value("${tripod.datasource.default.test-on-borrow:false}") boolean testOnBorrow
+			, @Value("${tripod.datasource.default.test-on-return:false}") boolean testOnReturn
+			, @Value("${tripod.datasource.default.pool-prepared-statements:true}") boolean poolPreparedStatements
+			, @Value("${tripod.datasource.default.max-pool-prepared-statement-per-connection-size:50}") int maxPoolPreparedStatementPerConnectionSize
+			, @Value("${tripod.datasource.default.max-open-prepared-statements:100}") int maxOpenPreparedStatements) throws SQLException {
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(userName);
+		dataSource.setPassword(password);
+		dataSource.setDefaultReadOnly(defaultReadOnly);
+		dataSource.setFilters(filters);
+		dataSource.setMaxActive(maxActive);
+		dataSource.setInitialSize(initialSize);
+		dataSource.setMaxWait(maxWaitMillis);
+		dataSource.setRemoveAbandoned(removeAbandoned);
+		dataSource.setRemoveAbandonedTimeout(removeAbandonedTimeout);
+		dataSource.setMinIdle(minIdle);
+		dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+		dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+		dataSource.setValidationQuery(validationQuery);
+		dataSource.setTestWhileIdle(testWhileIdle);
+		dataSource.setTestOnBorrow(testOnBorrow);
+		dataSource.setTestOnReturn(testOnReturn);
+		dataSource.setPoolPreparedStatements(poolPreparedStatements);
+		dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+		dataSource.setMaxOpenPreparedStatements(maxOpenPreparedStatements);
+		return dataSource;
+	}
 
 	@ConditionalOnMissingBean(name = "mappingLocations")
 	@Bean(name = "mappingLocations")
