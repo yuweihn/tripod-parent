147251bbb0177fc440c7caa7938d3167d80706ba
yuwei <yuwei@yuweix.com>
2023-08-07 21:19:12 +0800

    2.0.5

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/mybatis/where/Criteria.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/mybatis/where/Criteria.java
@@ -2,6 +2,7 @@ package com.yuweix.tripod.dao.mybatis.where;
 
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,10 +45,25 @@ public class Criteria implements Serializable {
 			throw new IllegalArgumentException("Invalid argument.");
 		}
 
-		if (value == null) {
+		String paramKey = "p" + hashCode +  "" + pindex;
+		if (operator.getCode().equals(Operator.in.getCode()) || operator.getCode().equals(Operator.nin.getCode())) {
+			if ((value == null) || !(value instanceof Collection)) {
+				return "";
+			} else {
+				Collection<?> col = (Collection<?>) value;
+				if (col.size() <= 0) {
+					if (operator.getCode().equals(Operator.in.getCode())) {
+						return " 1 = 0 ";
+					} else {
+						return " 1 = 1 ";
+					}
+				} else {
+					return key + " " + operator.getCode() + " <foreach item='item' collection='criteria.params." + paramKey + "' open='(' separator=',' close=')'>#{item}</foreach> ";
+				}
+			}
+		} else if (value == null) {
 			return key + " " + operator.getCode() + " ";
 		} else {
-			String paramKey = "p" + hashCode +  "" + pindex;
 			params.put(paramKey, value);
 			return key + " " + operator.getCode() + " #{criteria.params." + paramKey + "} ";
 		}
@@ -70,12 +86,18 @@ public class Criteria implements Serializable {
 		String criterionSql = createCriterionSql(key, operator, value, this.params, this.hashCode(), ++this.pindex);
 		return add(Connector.and, criterionSql);
 	}
+	public Criteria and(String sql) {
+		return add(Connector.and, sql);
+	}
 	public Criteria or(String key, Operator operator) {
 		return or(key, operator, null);
 	}
 	public Criteria or(String key, Operator operator, Object value) {
 		String criterionSql = createCriterionSql(key, operator, value, this.params, this.hashCode(), ++this.pindex);
 		return add(Connector.or, criterionSql);
+	}
+	public Criteria or(String sql) {
+		return add(Connector.or, sql);
 	}
 
 	private Criteria add(Connector connector, Criteria criteria) {
