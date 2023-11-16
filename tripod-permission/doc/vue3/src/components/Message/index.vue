65f4f5f7908d3411b39fb99dc76e8b94a6a01173
yuwei <yuwei@yuweix.com>
2023-11-16 19:13:03 +0800

    1.0.10

---
+++ tripod-permission/doc/vue3/src/components/Message/index.vue
@@ -0,0 +1,27 @@
+<template>
+<el-dialog title="信息" v-model="formVisible" width="650px" :close-on-click-modal="true" append-to-body draggable>
+	<div><pre style="white-space: pre-wrap;">{{content}}</pre></div>
+	<div slot="footer" class="dialog-footer" style="margin-top: 50px;">
+		<el-button @click.native="formVisible = false" :icon="Close"> 关闭</el-button>
+	</div>
+</el-dialog>
+</template>
+
+<script setup>
+import {Close} from '@element-plus/icons-vue';
+const formVisible = ref(false);
+const content = ref(null);
+
+function show(ctt) {
+    content.value = ctt;
+    formVisible.value = true;
+}
+defineExpose({
+    show
+})
+</script>
+
+<style scoped>
+
+</style>
+
