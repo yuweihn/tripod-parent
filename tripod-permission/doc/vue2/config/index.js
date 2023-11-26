9ffd789fb38b22310481ba7f9b92e5d127b344df
yuwei <yuwei@yuweix.com>
2023-11-26 12:18:36 +0800

    2.0.6

--- tripod-permission/doc/vue2/config/index.js
+++ tripod-permission/doc/vue2/config/index.js
@@ -46,7 +46,7 @@ module.exports = {
   build: {
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
-    //index: path.resolve(__dirname, '../../src/main/resources/index.html'),
+    //index: path.resolve(__dirname, '../../src/main/resources/static/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
