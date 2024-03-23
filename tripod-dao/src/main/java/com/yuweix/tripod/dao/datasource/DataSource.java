7077f59d8f4002420f32e3c55f8b27ad0d865b55
yuwei <yuwei@yuweix.com>
2024-03-23 17:23:34 +0800

    1.0.16

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DataSource.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DataSource.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface DataSource {
     String value() default "";
 }
