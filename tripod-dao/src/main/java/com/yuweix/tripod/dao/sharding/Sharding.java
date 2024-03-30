a73cb9f570a9bad950fe9ebd2f9a224891aa726f
yuwei <yuwei@yuweix.com>
2024-03-30 18:45:02 +0800

    1.0.17

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/Sharding.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/Sharding.java
@@ -3,15 +3,15 @@ package com.yuweix.tripod.dao.sharding;
 
 import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.FIELD;
+
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 
 /**
  * @author yuwei
  */
-@Target({METHOD, FIELD})
+@Target({METHOD, FIELD, PARAMETER})
 @Retention(RUNTIME)
 public @interface Sharding {
     /**
