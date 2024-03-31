1ca7efc8e1f5c475ed922099111ecb1e7318149b
yuwei <yuwei@yuweix.com>
2024-03-31 16:07:42 +0800

    1.0.17

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/Strategy.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/Strategy.java
@@ -12,8 +12,9 @@ public interface Strategy {
     }
 
     /**
+     * @param logicName                  逻辑库/表名
      * @param shardingVal                分片字段的值
      * @return   返回分片。如：0000,0001等等
      */
-    <T>String getShardingIndex(String tableName, T shardingVal);
+    <T>String getShardingIndex(String logicName, T shardingVal);
 }
