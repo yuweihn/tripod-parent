ef51f19d5fda4f5af708ae3a1da8f9f179d8f29c
yuwei <yuwei@yuweix.com>
2024-04-01 22:45:58 +0800

    2.0.9

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DynamicDataSourceAspect.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DynamicDataSourceAspect.java
@@ -41,17 +41,20 @@ public class DynamicDataSourceAspect {
     }
 
     private <T extends Annotation>T getAnnotation(JoinPoint point, Class<T> clz) {
+        Class<?> targetClz = point.getTarget().getClass();
         Method method = ((MethodSignature) point.getSignature()).getMethod();
         if (method == null) {
             return null;
         }
-        T t = null;
         if (method.isAnnotationPresent(clz)) {
-            t = method.getAnnotation(clz);
+            return method.getAnnotation(clz);
         }
-        if (t == null && method.getDeclaringClass().isAnnotationPresent(clz)) {
-            t = method.getDeclaringClass().getAnnotation(clz);
+        if (method.getDeclaringClass().isAnnotationPresent(clz)) {
+            return method.getDeclaringClass().getAnnotation(clz);
         }
-        return t;
+        if (targetClz.isAnnotationPresent(clz)) {
+            return targetClz.getAnnotation(clz);
+        }
+        return null;
     }
 }
