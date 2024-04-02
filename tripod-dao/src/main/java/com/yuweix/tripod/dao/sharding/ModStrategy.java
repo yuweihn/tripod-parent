24baacf1cb846e35288970fc0cbd21d5f3bbe647
wei.yu <weiy.yu@os.51job.com>
2024-04-02 12:24:23 +0800

    2.0.9

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/ModStrategy.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/ModStrategy.java
@@ -3,15 +3,15 @@ package com.yuweix.tripod.dao.sharding;
 
 
 /**
- * 分片策略-取模
+ * 分表策略-取模
  * @author yuwei
  */
 public class ModStrategy implements Strategy {
     @Override
-    public <T>String getShardingIndex(String logicName, T shardingVal) {
-        TableConfig conf = getTableConf(logicName);
+    public <T>String getShardingIndex(String tableName, T shardingVal) {
+        TableConfig conf = getTableConf(tableName);
         if (conf == null) {
-            throw new RuntimeException("[" + logicName + "]'s sharding-conf is required.");
+            throw new RuntimeException("[" + tableName + "]'s sharding-conf is required.");
         }
         return String.format("%0" + conf.getSuffixLength() + "d", hash(shardingVal) % conf.getShardingSize());
     }
