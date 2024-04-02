92f733c88a4d46b1042d06b71ba3b3ed58424775
wei.yu <weiy.yu@os.51job.com>
2024-04-02 16:34:57 +0800

    1.0.17

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DataSourceWrapper.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DataSourceWrapper.java
@@ -2,58 +2,36 @@ package com.yuweix.tripod.dao.datasource;
 
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 /**
+ *
+ {
+     "logicList": [{
+         "logicName": "gateway",
+         "shardList": [{
+             "index": 0,
+             "dataSource": "@@######"
+         }, {
+             "index": 1,
+             "dataSource": "@@######"
+         }]
+     }]
+ }
+ *
  * @author yuwei
  */
 public class DataSourceWrapper {
-    private String logicName;
-
-    private List<DataSource> dataSourceList;
-
-
-    public DataSourceWrapper(String logicName) {
-        this.logicName = logicName;
-    }
-
-    public DataSourceWrapper setDataSourceList(List<DataSource> dataSourceList) {
-        this.dataSourceList = dataSourceList;
-        return this;
-    }
-
-    public DataSourceWrapper addDataList(DataSource dataSource) {
-        if (this.dataSourceList == null) {
-            this.dataSourceList = new ArrayList<>();
-        }
-        this.dataSourceList.add(dataSource);
-        return this;
-    }
-
-    private String getKey(int index) {
-        return logicName + "_" + String.format("%0" + suffixLength + "d", index);
-    }
+    private List<LogicDatabase> logicList;
 
     private static class LogicDatabase {
+        private String logicName;
+        private List<DatabaseShard> shardList;
 
-    }
-
-    public Map<Object, Object> getTargetDataSourcesMap() {
-        Map<Object, Object> map = new HashMap<>();
-        if (dataSourceList == null || dataSourceList.isEmpty()) {
-            return map;
+        private static class DatabaseShard {
+            private int index;
+            private DataSource dataSource;
         }
-        if (dataSourceList.size() == 1) {
-            map.put(logicName, dataSourceList.get(0));
-        } else {
-            for (int i = 0, sz = dataSourceList.size(); i < sz; i++) {
-                map.put(this.getKey(i), dataSourceList.get(i));
-            }
-        }
-        return map;
     }
 }
