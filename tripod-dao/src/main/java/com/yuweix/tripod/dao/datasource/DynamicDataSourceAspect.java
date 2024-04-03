27db077bb3568fdddd2a427a73f923d11e57a0e6
yuwei <yuwei@yuweix.com>
2024-04-03 22:49:18 +0800

    2.0.9

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DynamicDataSourceAspect.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/datasource/DynamicDataSourceAspect.java
@@ -1,8 +1,6 @@
 package com.yuweix.tripod.dao.datasource;
 
 
-import com.yuweix.tripod.dao.sharding.DatabaseStrategy;
-import com.yuweix.tripod.dao.sharding.ShardDatabase;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -12,7 +10,6 @@ import org.aspectj.lang.reflect.MethodSignature;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 
 
 /**
@@ -36,44 +33,12 @@ public class DynamicDataSourceAspect {
         }
         String logicDatabaseName = annotation.value();
 
-        int shardingValIndex = getShardDatabaseAnnIndex(point);
-        if (shardingValIndex < 0) {
-            return point.proceed();
-        }
-
-        Method method = ((MethodSignature) point.getSignature()).getMethod();
-        Parameter[] params = method.getParameters();
-        ShardDatabase shardDatabase = params[shardingValIndex].getAnnotation(ShardDatabase.class);
-
         try {
-            String physicalDatabase = determinePhysicalDatabase(logicDatabaseName, point.getArgs()[shardingValIndex], shardDatabase.strategy());
-            DataSourceContextHolder.setDataSource(physicalDatabase);
+            DataSourceContextHolder.setDataSource(logicDatabaseName);
             return point.proceed();
         } finally {
             DataSourceContextHolder.removeDataSource();
         }
-    }
-
-    private String determinePhysicalDatabase(String logicDatabaseName, Object shardingVal, Class<? extends DatabaseStrategy> strategyClz) {
-        return logicDatabaseName;
-    }
-
-    /**
-     * 在当前方法的参数中，获取到分库注解
-     * @return 返回注解在参数列表中的index
-     */
-    private int getShardDatabaseAnnIndex(ProceedingJoinPoint point) {
-        Method method = ((MethodSignature) point.getSignature()).getMethod();
-        Parameter[] params = method.getParameters();
-        if (params == null || params.length <= 0) {
-            return -1;
-        }
-        for (int i = 0, len = params.length; i < len; i++) {
-            if (params[i].isAnnotationPresent(ShardDatabase.class)) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     /**
