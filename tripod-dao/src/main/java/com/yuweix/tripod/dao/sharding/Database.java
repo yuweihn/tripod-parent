c3f8a876f5fc003b7315831b51d797ab175edc3c
yuwei <yuwei@yuweix.com>
2024-04-04 11:13:20 +0800

    2.0.9

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/Database.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/Database.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 
 
 /**
- * 分库时使用，注解在方法参数上，参数作为分库字段，如果该参数为对象，该对象中必须包含一个属性，该属性有Sharding注解
+ * 分库时使用，注解在方法参数上，参数作为分库字段，如果该参数为对象，该对象中必须包含一个属性，该属性有{@link Sharding}注解
  * @author yuwei
  */
 @Inherited
--- tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/DatabaseHelper.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/sharding/DatabaseHelper.java
@@ -1,12 +1,39 @@
 package com.yuweix.tripod.dao.sharding;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 
 public abstract class DatabaseHelper {
     /**
-     * 检查入参，如果包含某个属性，该属性有Sharding注解，则返回该属性的值
+     * 获取含有{@link Database}注解的参数对象
+     */
+    public static Object getDatabaseArg(ProceedingJoinPoint point) {
+        Method method = ((MethodSignature) point.getSignature()).getMethod();
+        Parameter[] params = method.getParameters();
+        if (params == null || params.length <= 0) {
+            return null;
+        }
+        int idx = -1;
+        for (int i = 0, len = params.length; i < len; i++) {
+            if (params[i].isAnnotationPresent(Database.class)) {
+                idx = i;
+                break;
+            }
+        }
+        if (idx < 0) {
+            return null;
+        }
+        return point.getArgs()[idx];
+    }
+
+    /**
+     * 检查入参，如果包含某个属性，该属性有{@link Sharding}注解，则返回该属性的值
      * @param obj
      * @return
      */
