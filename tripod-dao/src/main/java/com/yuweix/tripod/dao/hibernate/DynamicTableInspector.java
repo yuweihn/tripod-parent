88249f479ed9e43fff124f49853e5c430d6c1e84
yuwei <yuwei@yuweix.com>
2024-04-06 17:36:56 +0800

    2.0.9

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/hibernate/DynamicTableInspector.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/hibernate/DynamicTableInspector.java
@@ -3,6 +3,8 @@ package com.yuweix.tripod.dao.hibernate;
 
 import com.yuweix.tripod.dao.sharding.DynamicTableTL;
 import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -10,17 +12,22 @@ import org.hibernate.resource.jdbc.spi.StatementInspector;
  * @author yuwei
  */
 public class DynamicTableInspector implements StatementInspector {
+	private static final Logger log = LoggerFactory.getLogger(DynamicTableInspector.class);
+
 	public DynamicTableInspector() {
 
 	}
 
 	@Override
 	public String inspect(String sql) {
+		log.info("Original SQL: {}", sql);
 		final String srcName = DynamicTableTL.getSrcName();
 		final String targetName = DynamicTableTL.getTargetName();
-		if (srcName == null || targetName == null) {
-			return sql;
+		String actualSql = sql;
+		if (srcName != null && targetName != null) {
+			actualSql = sql.replaceAll(srcName, targetName);
 		}
-		return sql.replaceAll(srcName, targetName);
+		log.info("Actual SQL: {}", actualSql);
+		return actualSql;
 	}
 }
