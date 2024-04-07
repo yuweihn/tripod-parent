f658bb598e140d919f7e7f3cfdfcc5c874fcad3c
wei.yu <weiy.yu@os.51job.com>
2024-04-07 17:41:07 +0800

    1.0.17

--- tripod-dao/src/main/java/com/yuweix/tripod/dao/PersistUtil.java
+++ tripod-dao/src/main/java/com/yuweix/tripod/dao/PersistUtil.java
@@ -237,8 +237,8 @@ public abstract class PersistUtil {
 	}
 
 	public static class FieldCol {
-		private String columnName;
-		private Field field;
+		private final String columnName;
+		private final Field field;
 
 		public FieldCol(String columnName, Field field) {
 			this.columnName = columnName;
