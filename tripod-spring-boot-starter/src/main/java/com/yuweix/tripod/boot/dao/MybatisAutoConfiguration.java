bec0ee332236988d5f3879685170f9bbd0cc0066
yuwei <yuwei@yuweix.com>
2024-04-14 21:05:10 +0800

    1.0.17

--- tripod-spring-boot-starter/src/main/java/com/yuweix/tripod/boot/dao/MybatisAutoConfiguration.java
+++ tripod-spring-boot-starter/src/main/java/com/yuweix/tripod/boot/dao/MybatisAutoConfiguration.java
@@ -6,6 +6,7 @@ import com.yuweix.tripod.data.cache.Cache;
 import com.yuweix.tripod.dao.springboot.MybatisConf;
 import com.yuweix.tripod.sequence.springboot.SequenceConf;
 import com.yuweix.tripod.sharding.springboot.ShardingConf;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -22,18 +23,21 @@ import org.springframework.context.annotation.Import;
 public class MybatisAutoConfiguration {
 	@ConditionalOnMissingBean(PersistCache.class)
 	@Bean(name = "persistCache")
-	public PersistCache persistCache(Cache cache) {
+	public PersistCache persistCache(@Autowired(required = false) Cache cache) {
 		return new PersistCache() {
 			@Override
 			public <T> boolean put(String key, T t, long timeout) {
-				return cache.put(key, t, timeout);
+				return cache != null && cache.put(key, t, timeout);
 			}
 			@Override
 			public <T> T get(String key) {
-				return cache.get(key);
+				return cache == null ? null : cache.get(key);
 			}
 			@Override
 			public void remove(String key) {
+				if (cache == null) {
+					return;
+				}
 				cache.remove(key);
 			}
 		};
