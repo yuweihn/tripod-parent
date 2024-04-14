5466ea23d1b1eb2a7e357233ac710a181beee913
yuwei <yuwei@yuweix.com>
2024-04-14 21:01:51 +0800

    2.0.9

--- tripod-spring-boot-starter/src/main/java/com/yuweix/tripod/boot/dao/HibernateAutoConfiguration.java
+++ tripod-spring-boot-starter/src/main/java/com/yuweix/tripod/boot/dao/HibernateAutoConfiguration.java
@@ -1,10 +1,14 @@
 package com.yuweix.tripod.boot.dao;
 
 
+import com.yuweix.tripod.dao.PersistCache;
 import com.yuweix.tripod.dao.springboot.HibernateConf;
+import com.yuweix.tripod.data.cache.Cache;
 import com.yuweix.tripod.sequence.springboot.SequenceConf;
 import com.yuweix.tripod.sharding.springboot.ShardingConf;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -16,5 +20,22 @@ import org.springframework.context.annotation.Import;
 @ConditionalOnProperty(name = "tripod.boot.dao.hibernate.enabled")
 @Import({ShardingConf.class, HibernateConf.class, SequenceConf.class})
 public class HibernateAutoConfiguration {
-
+    @ConditionalOnMissingBean(PersistCache.class)
+    @Bean(name = "persistCache")
+    public PersistCache persistCache(Cache cache) {
+        return new PersistCache() {
+            @Override
+            public <T> boolean put(String key, T t, long timeout) {
+                return cache.put(key, t, timeout);
+            }
+            @Override
+            public <T> T get(String key) {
+                return cache.get(key);
+            }
+            @Override
+            public void remove(String key) {
+                cache.remove(key);
+            }
+        };
+    }
 }
