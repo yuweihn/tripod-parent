1b45396ca7a9c0a9cc05f108c130bb39273b38b8
yuwei <yuwei@yuweix.com>
2024-08-22 22:17:57 +0800

    1.0.19

--- tripod-core/src/main/java/com/yuweix/tripod/core/mq/rabbit/RabbitCallback.java
+++ tripod-core/src/main/java/com/yuweix/tripod/core/mq/rabbit/RabbitCallback.java
@@ -4,6 +4,9 @@ package com.yuweix.tripod.core.mq.rabbit;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 
 
+/**
+ * @author yuwei
+ **/
 public interface RabbitCallback {
     void call(CorrelationData correlationData, boolean ack, String cause);
 }
