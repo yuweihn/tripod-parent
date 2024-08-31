6d15330738d4d08145e1f9392deffa6314c26e2d
yuwei <yuwei@yuweix.com>
2024-08-31 12:48:43 +0800

    2.0.11

---
+++ tripod-core/src/main/java/com/yuweix/tripod/core/office/excel/annotation/ExcelKey.java
@@ -0,0 +1,16 @@
+package com.yuweix.tripod.core.office.excel.annotation;
+
+
+import java.lang.annotation.*;
+
+
+/**
+ * @author yuwei
+ */
+@Target({ElementType.TYPE, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ExcelKey {
+	String title() default "";
+	int order() default Integer.MAX_VALUE;
+}
---
+++ tripod-core/src/main/java/com/yuweix/tripod/core/office/excel/writer/PoiExcelWriter.java
@@ -0,0 +1,244 @@
+package com.yuweix.tripod.core.office.excel.writer;
+
+
+import com.yuweix.tripod.core.office.excel.annotation.ExcelKey;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletResponse;
+import java.beans.PropertyDescriptor;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+/**
+ * @author yuwei
+ */
+public abstract class PoiExcelWriter {
+	private static final Logger log = LoggerFactory.getLogger(PoiExcelWriter.class);
+	private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	private static final int DEFAULT_FONT_HEIGHT = 20;
+
+
+	/**
+	 * 导出数据
+	 * @param dataList 数据
+	 */
+	public static<T> byte[] export(Class<T> clz, List<T> dataList) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		export(clz, dataList, out);
+		byte[] data = out.toByteArray();
+
+		try {
+			out.close();
+		} catch (IOException e) {
+			log.error("", e);
+		}
+		return data;
+	}
+
+	public static<T> void export(Class<T> clz, List<T> dataList, String fileName, HttpServletResponse resp) {
+		resp.setContentType("application/vnd.ms-excel");
+		resp.setCharacterEncoding("utf-8");
+		resp.setHeader("Content-disposition", "attachment;filename=" + fileName);
+		resp.setHeader("_filename", fileName);
+		resp.setHeader("Access-Control-Expose-Headers", "_filename");
+		try {
+			export(clz, dataList, resp.getOutputStream());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * 导出数据到输出流
+	 * @param dataList 数据
+	 */
+	public static<T> void export(Class<T> clz, List<T> dataList, OutputStream out) {
+		log.info("list size: {}", dataList == null ? 0 : dataList.size());
+		SXSSFWorkbook workbook = null;
+		try {
+			workbook = new SXSSFWorkbook();
+			workbook.setCompressTempFiles(true);
+			/**
+			 * 表头样式
+			 */
+			CellStyle titleStyle = workbook.createCellStyle();
+			titleStyle.setAlignment(HorizontalAlignment.CENTER);
+			Font titleFont = workbook.createFont();
+			titleFont.setFontHeightInPoints((short) DEFAULT_FONT_HEIGHT);
+			titleFont.setBold(true);
+			titleStyle.setFont(titleFont);
+			titleStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+			titleStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+			SXSSFSheet sheet = workbook.createSheet();
+			sheet.trackAllColumnsForAutoSizing();
+
+			/**
+			 * 输出头部
+			 **/
+			List<String> headList = dataList != null && !dataList.isEmpty()
+					? getOutputHeadList(dataList.get(0))
+					: getOutputHeadList(clz);
+			SXSSFRow headRow = sheet.createRow(0);
+			for (int i = 0; i < headList.size(); i++) {
+				String head = headList.get(i);
+				SXSSFCell cell = headRow.createCell(i);
+				cell.setCellValue(head);
+				cell.setCellStyle(titleStyle);
+				sheet.autoSizeColumn(i, true);
+			}
+
+			if (dataList != null && !dataList.isEmpty()) {
+				List<String> keyList = getOutputKeyList(dataList.get(0));
+				/**
+				 * 输出数据部分
+				 **/
+				for (int i = 0; i < dataList.size(); i++) {
+					T t = dataList.get(i);
+					SXSSFRow dataRow = sheet.createRow(i + 1);
+					List<Object> dList = getOutputDataList(keyList, t);
+					for (int j = 0; j < dList.size(); j++) {
+						SXSSFCell cell = dataRow.createCell(j);
+						setCellValue(cell, dList.get(j));
+						sheet.autoSizeColumn(j, true);
+					}
+				}
+			}
+
+			workbook.write(out);
+		} catch (Exception e) {
+			log.error("", e);
+		} finally {
+			if (workbook != null) {
+				try {
+					workbook.close();
+				} catch (IOException e) {
+					log.error("", e);
+				}
+			}
+		}
+	}
+
+	private static<T> List<String> getOutputHeadList(T t) {
+		List<String> list = new ArrayList<>();
+		if (Map.class.isAssignableFrom(t.getClass())) {
+			Map<?, ?> map = (Map<?, ?>) t;
+			for (Object o : map.keySet()) {
+				list.add(o.toString());
+			}
+			return list;
+		} else {
+			return getOrderedOutputHeadList(t.getClass().getDeclaredFields());
+		}
+	}
+
+	private static<T> List<String> getOutputHeadList(Class<T> clz) {
+		if (clz == null) {
+			return new ArrayList<>();
+		}
+		return getOrderedOutputHeadList(clz.getDeclaredFields());
+	}
+
+	private static List<String> getOrderedOutputHeadList(Field[] fields) {
+		List<String> list = new ArrayList<>();
+		if (fields == null || fields.length <= 0) {
+			return list;
+		}
+		Set<Field> fieldSet = new TreeSet<>(new Comparator<Field>() {
+			@Override
+			public int compare(Field f1, Field f2) {
+				ExcelKey excelKey1 = f1.getAnnotation(ExcelKey.class);
+				ExcelKey excelKey2 = f2.getAnnotation(ExcelKey.class);
+				if (excelKey1 == null || excelKey2 == null) {
+					return -1;
+				}
+				if (excelKey1.order() == excelKey2.order()) {
+					return 1;
+				}
+				return excelKey1.order() - excelKey2.order();
+			}
+		});
+		fieldSet.addAll(Arrays.asList(fields));
+		for (Field field: fieldSet) {
+			ExcelKey excelKey = field.getAnnotation(ExcelKey.class);
+			if (excelKey != null) {
+				list.add(excelKey.title() == null || "".equals(excelKey.title().trim()) ? field.getName() : excelKey.title().trim());
+			}
+		}
+		return list;
+	}
+
+	private static<T> List<String> getOutputKeyList(T t) {
+		List<String> list = new ArrayList<>();
+		if (Map.class.isAssignableFrom(t.getClass())) {
+			Map<?, ?> map = (Map<?, ?>) t;
+			for (Object o : map.keySet()) {
+				list.add(o.toString());
+			}
+		} else {
+			Field[] fields = t.getClass().getDeclaredFields();
+			if (fields != null && fields.length > 0) {
+				for (Field field: fields) {
+					ExcelKey excelKeyAno = field.getAnnotation(ExcelKey.class);
+					if (excelKeyAno != null) {
+						list.add(field.getName());
+					}
+				}
+			}
+		}
+
+		return list;
+	}
+
+	private static<T> List<Object> getOutputDataList(List<String> keyList, T t) {
+		List<Object> list = new ArrayList<>();
+		if (keyList == null || keyList.size() <= 0) {
+			return list;
+		}
+
+		if (Map.class.isAssignableFrom(t.getClass())) {
+			Map<?, ?> map = (Map<?, ?>) t;
+			for (String key: keyList) {
+				list.add(map.get(key));
+			}
+		} else {
+			for (String key: keyList) {
+				PropertyDescriptor pd = null;
+				try {
+					pd = new PropertyDescriptor(key, t.getClass());
+					Method getMethod = pd.getReadMethod();
+					Object o = getMethod.invoke(t);
+					list.add(o);
+				} catch (Exception e) {
+					log.error("", e);
+				}
+			}
+		}
+		return list;
+	}
+
+	private static void setCellValue(Cell cell, Object value) {
+		if (value instanceof Number) {
+			cell.setCellValue(value == null ? 0 : Double.parseDouble(value.toString()));
+		} else if (value instanceof Boolean) {
+			cell.setCellValue(value != null && Boolean.parseBoolean(value.toString()));
+		} else if (value instanceof Date) {
+			cell.setCellValue(value == null ? "" : new SimpleDateFormat(DEFAULT_DATE_FORMAT).format(value));
+		} else {
+			cell.setCellValue(value == null ? "" : value.toString());
+		}
+	}
+}
+
